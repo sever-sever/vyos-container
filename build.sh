bc142e6b0000ca8609736810bff263f90eeb9f8b
Viacheslav Hletenko <seversss265@gmail.com>
2026-06-11 06:37:38 +0300

    Update ISO JSON URL to use rolling version

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Fetch JSON
 echo "Fetching latest VyOS ISO URL..."
-ISO_JSON_URL="https://raw.githubusercontent.com/vyos/vyos-nightly-build/refs/heads/current/version.json"
+ISO_JSON_URL="https://raw.githubusercontent.com/vyos/vyos-nightly-build/rolling/version.json"
 ISO_URL=$(curl -s "${ISO_JSON_URL}" | jq -r '.[0].url')
 
 echo "Building Docker image using ISO: ${ISO_URL}"
